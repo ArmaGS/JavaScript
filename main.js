658807e81d380fab743df2f69dcf7cb1cabbb141
ArmaGS <agaytan@somosproper.com>
2021-12-20 22:05:20 -0600

    Se agrega proyecto

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#c1").mouseleave(function(){
+        $("#c2").hide()
+    });
+    $("#c1").mouseenter(function(){
+        $("#c2").show()
+    });
+
+    $("#c3").mouseleave(function(){
+        $("#c4").hide()
+    });
+    $("#c3").mouseenter(function(){
+        $("#c4").show()
+    });
+
+    $("#c5").mouseleave(function(){
+        $("#c6").hide()
+    });
+    $("#c5").mouseenter(function(){
+        $("#c6").show()
+    });
+
+    $("#c7").mouseleave(function(){
+        $("#c8").hide()
+    });
+    $("#c7").mouseenter(function(){
+        $("#c8").show()
+    });
+
+    $("#c9").mouseleave(function(){
+        $("#c10").hide()
+    });
+    $("#c9").mouseenter(function(){
+        $("#c10").show()
+    });
+
+   
+
+});
